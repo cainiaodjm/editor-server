ef4a656cc5f824b6c0789e2739bbafa3b00073d5
cainiaodjm <cainiaodjm@163.com>
2021-09-29 19:35:04 +0800

    update

---
+++ src/middlewares/jwt.js
@@ -0,0 +1,12 @@
+const jwtKoa = require('koa-jwt')
+
+const {JWT_SECRET,JWT_IGNORE_PATH} = require('../config/constant')
+
+
+module.exports = jwtKoa({
+    secret:JWT_SECRET,
+    cookie:'jwt_token'
+}).unless({
+    //定义那些路由忽略 jwt 验证
+    path:JWT_IGNORE_PATH
+})
