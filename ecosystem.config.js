2db216461fb200c0f2383409f1da07f7f5c6be2c
cainiaodjm <cainiaodjm@163.com>
2021-10-25 17:10:28 +0800

    添加docker compose 文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,18 @@
-const os = require('os')
-const cpuCoreLength = os.cpus().length
+const os = require("os");
+
+const cpuCoreLength = os.cpus().length;
 module.exports = {
-  apps : [{
-    name   : "biz-editor-server",
-    script : "./bin/www.js",
-    instances:1,
-    ignore_watch:['node_modules','__test__','logs'],
-    error_file:'./logs/err.log',
-    out_file:'./logs/out.log',
-    log_date_format:'YYYY-MM-DD HH:mm Z',
-    combine_logs:true,
-    max_memory_restart:'300M'
-  }]
-}
+  apps: [
+    {
+      name: "biz-editor-server",
+      script: "./bin/www.js",
+      instances: 1,
+      ignore_watch: ["node_modules", "__test__", "logs"],
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      combine_logs: true,
+      max_memory_restart: "300M",
+    },
+  ],
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const router = require("koa-router")();
-const testMysqlConn = require("../db/mysql2");
+
 const packageInfo = require("../../package.json");
 const ENV = require("../utils/env");
-const { WorkModel } = require("../models/WorkModel");
-const { cacheGet, cacheSet } = require("../cache/index");
+// const testMysqlConn = require("../db/mysql2");
+// const { WorkModel } = require("../models/WorkModel");
+// const { cacheGet, cacheSet } = require("../cache/index");
 
 router.get("/", async (ctx, next) => {
   await ctx.render("index", {
@@ -21,29 +22,34 @@ router.get("/json", async (ctx, next) => {
   };
 });
 router.get("/api/db-check", async (ctx) => {
-  // 测试mysql 连接
-  const mysqlRows = await testMysqlConn();
-  // 测试mongodb连接
-  let mongodbConn;
-  try {
-    mongodbConn = true;
-    await WorkModel.findOne();
-  } catch (error) {
-    mongodbConn = false;
-  }
+  // // 测试mysql 连接
+  // const mysqlRows = await testMysqlConn();
+  // // 测试mongodb连接
+  // let mongodbConn;
+  // try {
+  //   mongodbConn = true;
+  //   await WorkModel.findOne();
+  // } catch (error) {
+  //   mongodbConn = false;
+  // }
   // 测试redis连接
-  cacheSet("test", "biz editor server Ok - by redis");
-  const redisTestVal = await cacheGet("test");
-  console.log(redisTestVal);
+  // cacheSet("test", "biz editor server Ok - by redis");
+  // const redisTestVal = await cacheGet("test");
+  // console.log(redisTestVal);
   ctx.body = {
     errno: 0,
     data: {
       name: "ediotr_server",
       version: packageInfo.version,
       ENV,
-      mysqlConn: mysqlRows.length > 0,
-      mongodbConn,
-      redisConn: redisTestVal,
+      // mysqlConn: mysqlRows.length > 0,
+      // mongodbConn,
+      // redisConn: redisTestVal,
+      // Dockerfile 环境变量
+      dockerFile: {
+        SERVER_NAME: process.env.SERVER_NAME,
+        AUTHOR_NAME: process.env.AUTHOR_NAME,
+      },
     },
   };
 });
