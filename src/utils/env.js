cc85f07cfc144464246acbc9c8de7eddeee59d9e
cainiaodjm <cainiaodjm@163.com>
2021-11-01 14:53:23 +0800

    update

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,7 +3,7 @@ console.log(`当前环境为${ENV}`);
 module.exports = {
   ENV,
   isProd: ENV === "production",
-  isProdDev: ENV === "prod_dev",
+  isProdDev: ENV === "prd_dev",
   isDev: ENV === "dev",
   isTest: ENV.indexOf("test") === 0,
   isTestLocal: ENV === "test_local",
